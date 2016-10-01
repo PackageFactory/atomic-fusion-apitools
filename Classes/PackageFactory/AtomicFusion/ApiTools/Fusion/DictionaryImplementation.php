7ed88d4f6b48572e06636bb10576549641307240
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-01 10:05:44 +0200

    chore(FusionObjects): Scaffold all planned Fusion Object classes

---
+++ Classes/PackageFactory/AtomicFusion/ApiTools/Fusion/DictionaryImplementation.php
@@ -0,0 +1,23 @@
+<?php
+namespace PackageFactory\AtomicFusion\ApiTools\Fusion;
+
+/**
+ * This file is part of the PackageFactory.AtomicFusion.ApiTools package
+ *
+ * (c) 2016 Wilhelm Behncke <wilhelm.behncke@googlemail.com>
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject;
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A Fusion Object to represent arbitrary dictionaries
+ */
+class DictionaryImplementation extends AbstractTypoScriptObject
+{
+
+}
