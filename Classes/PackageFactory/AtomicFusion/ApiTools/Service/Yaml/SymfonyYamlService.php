435c2f5ec5a9d988fe7f2642ebdec7fdd9980475
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-01 10:36:17 +0200

    feat(Service.YAML): Implement symfony-driven yaml service

---
+++ Classes/PackageFactory/AtomicFusion/ApiTools/Service/Yaml/SymfonyYamlService.php
@@ -0,0 +1,48 @@
+<?php
+namespace PackageFactory\AtomicFusion\ApiTools\Service\Yaml;
+
+/**
+ * This file is part of the PackageFactory.AtomicFusion.ApiTools package
+ *
+ * (c) 2016 Wilhelm Behncke <wilhelm.behncke@googlemail.com>
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Use Symfonys YAML parser implementation
+ */
+class SymfonyYamlService implements ServiceInterface
+{
+	use CheckJsonSerializableTrait;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function parse($yamlString)
+	{
+		try {
+			return Yaml::parse($yamlString);
+		} catch (\Exception $e) {
+			throw new Exception('Could not parse string', 1475310795);
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function stringify($jsonSerializable)
+	{
+		$this->checkForYamlExtension();
+		
+		try {
+			return Yaml::dump($yamlString);
+		} catch (\Exception $e) {
+			throw new Exception('Failed to convert value', 1475310803);
+		}
+	}
+}
